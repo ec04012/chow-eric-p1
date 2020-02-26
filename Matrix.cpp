cac25e98dc5ede819475de67c738fc406583073d
ec04012 <ec04012@uga.edu>
2020-02-26 14:22:49 -0500

    Added temp return statements to Matrix

--- Matrix.cpp
+++ Matrix.cpp
@@ -2,28 +2,51 @@ typedef unsigned int uint;
 
 #include "Matrix.h"
 
-double **arr;
+double** arr;
+double tempX = 0;
 Matrix::Matrix(uint rows, uint cols) {}
-Matrix::Matrix(double **values, int w, int h) {}
-
-Matrix::Matrix(const Matrix &m) {}
+Matrix::Matrix(double** values, int w, int h) {}
+Matrix::Matrix(const Matrix& m) {}
 Matrix::~Matrix() {}
 
-Matrix Matrix::add(double s) const {}
-Matrix Matrix::add(const Matrix &m) const {}
+Matrix Matrix::add(double s) const {
+	return Matrix(0, 0);
+}
+Matrix Matrix::add(const Matrix& m) const {
+	return Matrix(0, 0);	
+}
 
-Matrix Matrix::subtract(double s) const {}
-Matrix Matrix::subtract(const Matrix &m) const {}
+Matrix Matrix::subtract(double s) const {
+	return Matrix(0, 0);
+}
+Matrix Matrix::subtract(const Matrix& m) const {
+	return Matrix(0, 0);
+}
 
-Matrix Matrix::multiply(double s) const {}
-Matrix Matrix::multiply(const Matrix &m) const {}
+Matrix Matrix::multiply(double s) const {
+	return Matrix(0, 0);
+}
+Matrix Matrix::multiply(const Matrix& m) const {
+	return Matrix(0, 0);
+}
 
-Matrix Matrix::divide(double s) const {}
-Matrix Matrix::t() const {}
+Matrix Matrix::divide(double s) const {
+	return Matrix(0, 0);
+}
+Matrix Matrix::t() const {
+	return Matrix(0, 0);
+}
 
-const uint Matrix::numRows() const {}
-const uint Matrix::numCols() const {}
+const uint Matrix::numRows() const {
+	return 1;
+}
+const uint Matrix::numCols() const {
+	return 1;
+}
 
-double & Matrix::at(uint row, uint col) {}
-const double & Matrix::at(uint row, uint col) const {}
-;
+double& Matrix::at(uint row, uint col) {
+	return tempX;
+}
+const double& Matrix::at(uint row, uint col) const {
+	return tempX;
+}
