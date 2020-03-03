d80847a14eb1629b0451014612087396c1b6e275
ec04012 <ec04012@uga.edu>
2020-03-03 14:29:18 -0500

    Tested at method

--- P1.cpp
+++ P1.cpp
@@ -6,6 +6,8 @@ using namespace std;
 int main() { 
     Matrix m (3,3);
     cout << m.at(0,0) << endl;
+    m.at(0,0) = 3;
+    cout << m.at(0,0) << endl;
     cout << "Hello World" << endl;
     return 0; 
 }
