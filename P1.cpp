d31c0c055250cca5f4da02575d567f23dba3f3c7
ec04012 <ec04012@uga.edu>
2020-03-03 14:07:45 -0500

    Added starter code to P1

--- P1.cpp
+++ P1.cpp
@@ -1,3 +1,11 @@
+#include<iostream>
+#include "Matrix.cpp"
+
+using namespace std;
+
 int main() { 
+    Matrix m (3,3);
+    cout << m.at(0,0) << endl;
+    cout << "Hello World" << endl;
     return 0; 
 }
